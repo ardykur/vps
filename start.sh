476a6cf367bca4185cbbf28bd0a50fcf6d596f60
ardykur <125230211+ardykur@users.noreply.github.com>
2026-06-19 09:25:15 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ vncserver :1 -geometry 1920x1080 -depth 24
 
 websockify --web=/usr/share/novnc/ 6080 localhost:5901 &
 
-echo "===================================="
-echo "Ubuntu GUI Container Ready"
-echo "URL : http://localhost:6080/vnc.html"
-echo "User : ardy28"
-echo "Password : 12345678"
-echo "VNC Password : 12345678"
-echo "===================================="
+echo "======================================"
+echo " Ubuntu GUI Container Ready"
+echo " noVNC URL    : http://localhost:6080/vnc.html"
+echo " User         : ardy28"
+echo " Password     : 12345678"
+echo " VNC Password : 12345678"
+echo "======================================"
 
 tail -f /dev/null
