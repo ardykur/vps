ce87fea8ebc2766b95eb06e5ac60da3f0464186e
ardykur <125230211+ardykur@users.noreply.github.com>
2026-06-19 09:18:20 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export DISPLAY=:1
+
+vncserver :1 -geometry 1920x1080 -depth 24
+
+websockify --web=/usr/share/novnc/ 6080 localhost:5901 &
+
+echo "===================================="
+echo "Ubuntu GUI Container Ready"
+echo "URL : http://localhost:6080/vnc.html"
+echo "User : ardy28"
+echo "Password : 12345678"
+echo "VNC Password : 12345678"
+echo "===================================="
+
+tail -f /dev/null
